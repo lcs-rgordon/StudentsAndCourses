462d5fbf8d6c177ad62d1ac0b178e1e4707bfe63
Russell Gordon <rgordon@lcs.on.ca>
2024-06-09 13:01:40 -0400

    Made the view to add a new student accessible from the students list view.

--- StudentsAndCourses/Views/StudentsListView.swift
+++ StudentsAndCourses/Views/StudentsListView.swift
@@ -12,12 +12,29 @@ struct StudentsListView: View {
     // MARK: Stored properties
     @State private var viewModel = StudentsListViewModel()
     
+    @State private var isNewStudentSheetShowing = false
+    
     // MARK: Computed properties
     var body: some View {
         NavigationStack {
             VStack {
                 List(viewModel.students) { student in
                     Text("\(student.lastName), \(student.firstName)")
+                }
+            }
+            .sheet(isPresented: $isNewStudentSheetShowing) {
+                AddStudentView(isShowing: $isNewStudentSheetShowing)
+                    .environment(viewModel) // Must insert view model into environment
+                    .presentationDetents([.fraction(0.3)])
+            }
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        isNewStudentSheetShowing = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+
                 }
             }
             .navigationTitle("Students")
