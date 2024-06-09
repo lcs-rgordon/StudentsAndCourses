48e97824ce1866f54b1397723ca256af149e1b09
Russell Gordon <rgordon@lcs.on.ca>
2024-06-09 10:15:41 -0400

    Created initial groups; also added Supabase package dependency and DB connection details.

---
+++ StudentsAndCourses/Helpers/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  StudentsAndCourses
+//
+//  Created by Russell Gordon on 2024-06-09.
+//
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://lyntxzkwkdobkywnylyy.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5bnR4emt3a2RvYmt5d255bHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NDAxMTYsImV4cCI6MjAzMzUxNjExNn0.HwgSNVuwbNOVBnsSFB7mXtTP7BioAqDzMmhnLeQ_a6U"
+)
