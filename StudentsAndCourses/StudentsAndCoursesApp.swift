63c609d804d3670095de335f01dadb935cddccab
Russell Gordon <rgordon@lcs.on.ca>
2024-06-09 10:06:32 -0400

    Initial Commit

---
+++ StudentsAndCourses/StudentsAndCoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentsAndCoursesApp.swift
+//  StudentsAndCourses
+//
+//  Created by Russell Gordon on 2024-06-09.
+//
+
+import SwiftUI
+
+@main
+struct StudentsAndCoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
