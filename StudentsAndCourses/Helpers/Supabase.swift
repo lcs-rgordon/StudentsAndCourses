230d4981502b4abdfd6bab2bcb3f0e7aca344c02
Russell Gordon <rgordon@lcs.on.ca>
2025-05-07 20:06:51 -0400

    Update to new database server for 2024-2025 school year (Mr. Gordon accidentally deleted the original database for this tutorial).

--- StudentsAndCourses/Helpers/Supabase.swift
+++ StudentsAndCourses/Helpers/Supabase.swift
@@ -9,6 +9,6 @@ import Foundation
 import Supabase
 
 let supabase = SupabaseClient(
-  supabaseURL: URL(string: "https://lyntxzkwkdobkywnylyy.supabase.co")!,
-  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5bnR4emt3a2RvYmt5d255bHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc5NDAxMTYsImV4cCI6MjAzMzUxNjExNn0.HwgSNVuwbNOVBnsSFB7mXtTP7BioAqDzMmhnLeQ_a6U"
+  supabaseURL: URL(string: "https://lugwpbpauaqvxrgihzyl.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1Z3dwYnBhdWFxdnhyZ2loenlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2NjIxODUsImV4cCI6MjA2MjIzODE4NX0.AwMWm4lrIT52caQOMB9kqK5rmHMmZ_ovz719MFo8_ow"
 )
