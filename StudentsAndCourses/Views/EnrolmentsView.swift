950c6efe5f0a52a384ae9767fe2b72fa3dfd0947
Russell Gordon <rgordon@lcs.on.ca>
2025-05-09 11:21:58 -0400

    Updated enrolments view code so it subscribes to database change notifications and updates it's view models.

--- StudentsAndCourses/Views/EnrolmentsView.swift
+++ StudentsAndCourses/Views/EnrolmentsView.swift
@@ -5,16 +5,29 @@
 //  Created by Russell Gordon on 2024-06-09.
 //
 
+import OSLog
 import SwiftUI
 
 struct EnrolmentsView: View {
+    
+    // MARK: Stored properties
+    
+    // Gain access to the shared change notifier so we can tell when
+    // a refresh of data on this view is required
+    @Environment(EnrollmentChangeNotifier.self) private var changeNotifier
+
+    // Create view models for each type of enrolment perspective
+    @State private var enrolmentsByCourseViewModel = EnrolmentsByCourseViewModel()
+    @State private var enrolmentsByStudentViewModel = EnrolmentsByStudentViewModel()
+    
+    // MARK: Computed properties
     var body: some View {
         NavigationStack {
             List {
 
                 NavigationLink {
                     EnrolmentsByCourseView(
-                        viewModel: EnrolmentsByCourseViewModel()
+                        viewModel: enrolmentsByCourseViewModel
                     )
                 } label: {
                     Text("By course")
@@ -22,12 +35,20 @@ struct EnrolmentsView: View {
 
                 NavigationLink {
                     EnrolmentsByStudentView(
-                        viewModel: EnrolmentsByStudentViewModel()
+                        viewModel: enrolmentsByStudentViewModel
                     )
                 } label: {
                     Text("By student")
                 }
 
+            }
+            .onChange(of: changeNotifier.changeCount) {
+
+                Logger.viewCycle.info("EnrolmentsView: Database change observed; updating view models.")
+                
+                enrolmentsByCourseViewModel.refresh()
+                enrolmentsByStudentViewModel.refresh()
+                
             }
             .navigationTitle("Enrolments")
         }
